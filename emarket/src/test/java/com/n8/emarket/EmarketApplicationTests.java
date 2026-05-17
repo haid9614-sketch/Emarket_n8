81c8214ec00dc8e7d3a7441a8eb8f8b639c439b6
haiDuong <haid9614@gmail.com>
2026-05-17 21:16:03 +0700

    khoi tao du an

---
+++ emarket/src/test/java/com/n8/emarket/EmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.n8.emarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
